1e00f136021cdfc750a5cdfeac4708635431ff5e
shomaprabhudessai <shoma3892@gmail.com>
2024-05-09 13:02:52 +0530

    Update discover_12.1.12.beta.js

--- UICCoreBeta/Discover_12_1_12/discover_12.1.12.beta.js
+++ UICCoreBeta/Discover_12_1_12/discover_12.1.12.beta.js
@@ -1,1 +1,30 @@
 console.log('12.1.12')
+var config={
+  config = {
+    core: {},
+    modules: {},
+    services: {}
+  }
+};
+
+config = {
+  core: {
+    blockedEl = [],
+    delay: 100.
+    modules: {
+      module1: { enabled: false },
+      module2: { enabled: false }
+    }
+  },
+  modules: {
+    module1: {
+      delay: '400'
+    },
+    module2: {
+      delay: '400'
+    }
+  }
+};
+(function() =>{
+  console.log('init')
+})()
