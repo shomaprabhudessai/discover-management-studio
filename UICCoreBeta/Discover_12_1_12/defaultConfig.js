df7166543570ee3d0a679d926222c7d35b2d20e0
shomaprabhudessai <shoma3892@gmail.com>
2024-05-15 10:03:29 +0530

    Create defaultConfig.js

---
+++ UICCoreBeta/Discover_12_1_12/defaultConfig.js
@@ -0,0 +1,25 @@
+config = {
+  core: {
+    coreConfig: ['123'],
+    screens: ['abc'],
+    modules: {
+      module1: {
+        enabled: true,
+        load: 300
+      }
+    }
+  },
+  services: {
+    service1: {
+      enabled: true;
+    }
+  },
+  modules: {
+    module1: {
+      config1: ['abc'],
+      config2: {
+        xyz: 200
+      }
+    }
+  }
+}
