b1cc7a558f0469b99e64c499c9289582913af800
shomaprabhudessai <shoma3892@gmail.com>
2024-05-15 11:56:27 +0530

    Update defaultConfig.js

--- UICCoreBeta/Discover_12_1_12/defaultConfig.js
+++ UICCoreBeta/Discover_12_1_12/defaultConfig.js
@@ -1,3 +1,20 @@
+// Default configuration
+(function () {
+  "use strict";
+  // DCX is expected to be defined in the global scope i.e. window.DCX
+  var config, isReinitialized = false, someObj,
+    DCX = window.DCX,
+    /**
+     * Due to issue with lack of change event propagation on legacy IE (W3C version of UIC)
+     * its mandatory to provide more specific configuration on IE6, IE7, IE8 and IE9 in legacy
+     * compatibility mode. For other browsers changeTarget can remain undefined as it is
+     * sufficient to listen to the change event at the document level.
+     */
+    changeTarget;
+
+  if (DCX.getFlavor() === "w3c" && DCX.utils.isLegacyIE) {
+    changeTarget = "input, select, textarea, button";
+  }
 config = {
   // adding core
   core: {
@@ -23,8 +40,21 @@ config = {
     module1: {
       config1: ['abc'],
       config2: {
-        xyz: 200
+        xyz: `200 ${someObj}`
       }
     }
   }
-}
+};
+
+//----------------------------------------------------------------------------------------------------------
+  //--------------------------------------------------------------------------------- Alternate FireFox Config
+  //----------------------------------------------------------------------------------------------------------
+  if (navigator.userAgent.indexOf("Firefox") !== -1) {
+    //------------------------- Work arond for FETCH issues
+    config.services.queue.asyncReqOnUnload = false;
+    config.services.queue.useFetch = false;
+  }
+  init();
+})();
+
+  
