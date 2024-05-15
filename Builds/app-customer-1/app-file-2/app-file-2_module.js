462fdd12a7f8e1587ddcf7968b78b921301d968d
shomaprabhudessai <shoma3892@gmail.com>
2024-05-15 15:11:57 +0530

    app-file 2 committed

---
+++ Builds/app-customer-1/app-file-2/app-file-2_module.js
@@ -0,0 +1,10 @@
+DCX.addModule('ajaxListener', function(){
+    var ajax;
+    var sum = 0;
+    return sum++;
+});
+DCX.addModule('utmLogger', function(){
+    var utmLogger;
+    var utmKey = '7587459jhhjfdugv;
+    return utmKey;
+});
