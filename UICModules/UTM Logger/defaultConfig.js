265481eca2665c29ed4f27a06656a6ca8da51cb9
shomaprabhudessai <shoma3892@gmail.com>
2024-07-31 17:05:48 +0530

    Update defaultConfig.js

--- UICModules/UTM Logger/defaultConfig.js
+++ UICModules/UTM Logger/defaultConfig.js
@@ -1,29 +1,7 @@
-var config = {
-  core: {
-      // Other core configurations
-      // .
-      // .
-      // .
-      modules: {
-          // Other core modules
-          // .
-          // .
-          // .
-         
-          utmLogger: {
-              enabled: true
-          }
-      }
-  },
-  modules: {
-      // Other module configurations
-      // .
-      // .
-      // .
-      utmLogger: {
-        logToConsole: true,
-        logToServer: false,
-        serverUrl: 'https://your-server-endpoint.com/log'
-    }
+{
+  "utmLogger": {
+    "utmEnabled": true,
+    "maxDays": 90,
+    "maxCampaigns": 50
   }
-};
+}
