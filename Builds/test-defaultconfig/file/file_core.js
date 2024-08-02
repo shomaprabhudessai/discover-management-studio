7eaf1ac22eac97f62b892726056c6eb391c4fdb4
shomaprabhudessai <shoma3892@gmail.com>
2024-08-02 13:28:32 +0530

    file committed

---
+++ Builds/test-defaultconfig/file/file_core.js
@@ -0,0 +1,27 @@
+//Version 12.1.12
+if (window.DCX) {
+    throw "Attempting to recreate DCX. Library may be included more than once on the page.";
+}
+window.DCX = (function () {
+  console.log('some script');
+})
+
+(function () {
+  console.log('IIFE');
+}());
+
+DCX.addService("service1", function () {
+   console.log('service 1');
+});
+DCX.addService("service2", function () {
+   console.log('service 2');
+});
+
+DCX.addModule("module1", function (context) {
+   console.log('module 1');
+});
+
+DCX.addModule("module2", function (context) {
+   console.log('module 2');
+});
+
